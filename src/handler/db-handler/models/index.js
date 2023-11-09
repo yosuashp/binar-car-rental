e6b2d3d61cf758372614c61a97ae22b1275829bc
yosuashp <sahatparulian85@gmail.com>
2023-11-09 14:04:46 +0700

    add: perubahan file

--- src/handler/db-handler/models/index.js
+++ src/handler/db-handler/models/index.js
@@ -1,55 +1,5 @@
-// 'use strict';
-
-// const fs = require('fs');
-// const path = require('path');
-// const Sequelize = require('sequelize');
-// const process = require('process');
-// const { exit } = require('process');
-// const basename = path.basename(__filename);
-// const env = process.env.NODE_ENV || 'development';
-// const config = require(__dirname + '/../config/config.json')[env];
-// const db = {};
-
-// let sequelize;
-// if (config.use_env_variable) {
-//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
-// } else {
-//   sequelize = new Sequelize(config.database, config.username, config.password, config);
-// }
-
-// sequelize
-//   .authenticate()
-//   .then(() => {
-//     console.log('Connection with db has been established successfully.');
-//   })
-//   .catch(err => {
-//     console.error('Unable to connect to the database:', err.message);
-//     exit(0)
-//   });
-
-// fs
-//   .readdirSync(__dirname)
-//   .filter(file => {
-//     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-//   })
-//   .forEach(file => {
-//     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-//     db[model.name] = model;
-//   });
-
-// Object.keys(db).forEach(modelName => {
-//   if (db[modelName].associate) {
-//     db[modelName].associate(db);
-//   }
-// });
-
-// db.sequelize = sequelize;
-// db.Sequelize = Sequelize;
-
-// module.exports = db;
-
 const knex = require('knex');
-const knexfile = require('../connection/connect_database'); // Adjust the path to your knexfile
+const knexfile = require('../connection/connect_database');
 const { Model } = require('objection');
 
 const db = knex(knexfile.development);
@@ -57,7 +7,7 @@ Model.knex(db);
 
 class Car extends Model {
   static get tableName() {
-    return 'Mobil'; // Replace with your actual table name
+    return 'Mobil';
   }
 }
 
